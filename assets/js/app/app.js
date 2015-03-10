4e560e1f82167bcc3557c3d90477efc5d3bf4476
Joseba Legarreta <josebalega@gmail.com>
2015-03-10 15:26:16 +0100

    Login with sockets first approach

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -10,4 +10,21 @@ $(function(){
   console.log('=== UP & RUNNING ===');
   console.log('====================');
 
+  // SOCKETS PROXY
+
+  app.proxy = function(method, url, data, onSuccess, onError, ctx){
+
+    var that = ctx || this;
+
+    method = method.toLowerCase();
+
+    io.socket[method](url, data, function(resData){
+
+      console.log(resData);
+
+    });
+
+  };
+
+
 });
--- assets/js/app/views/loginForm.js
+++ assets/js/app/views/loginForm.js
@@ -5,13 +5,16 @@ $(function(){
     el: $('fieldset[data-control=login]'),
 
     events: {
-
+      'keypress input'    : 'keyPressed',
+      'click input#login' : 'doLogin'
     },
 
     initialize: function(){
       if(!this.$el.length) return null;
       this.listenTo(Backbone, 'showLoginForm', this.showLoginForm);
       this.listenTo(Backbone, 'hideLoginForm', this.hideLoginForm);
+      this.$email    = this.$('input[type=email]');
+      this.$password = this.$('input[type=password]');
     },
 
     showLoginForm: function(){
@@ -20,6 +23,20 @@ $(function(){
 
     hideLoginForm: function(){
       this.$el.addClass('hidden');
+    },
+
+    // USER EVENTS
+
+    keyPressed: function(event){
+      if(event.keyCode === 13) return this.doLogin();
+    },
+
+    doLogin: function(){
+      var data = {
+        email   : this.$email.val(),
+        password: this.$password.val()
+      };
+      app.proxy('POST', '/login', data);
     }
 
   });
